18abf24a15a2c831281821474c75fd54523fadee
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2014-07-20 18:10:27 -0430

    Rediseno en BD para API, falta determinar incorporacion de datos de estudiantes asociada a oferta academica

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,16 +1,33 @@
 # Be sure to restart your server when you modify this file.
 
-# Add new inflection rules using the following format. Inflections
-# are locale specific, and you may define rules for as many different
-# locales as you wish. All of these examples are active by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
+# Add new inflection rules using the following format
+# (all these examples are active by default):
+ActiveSupport::Inflector.inflections do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
-# end
 
+  inflect.plural /([aeiou])([A-Z]|_|$)/, '\1s\2'
+  inflect.plural /([rlnd])([A-Z]|_|$)/, '\1es\2'
+  inflect.plural /([aeiou])([A-Z]|_|$)([a-z]+)([rlnd])($)/, '\1s\2\3\4es\5'
+  inflect.plural /([rlnd])([A-Z]|_|$)([a-z]+)([aeiou])($)/, '\1es\2\3\4s\5'
+  inflect.singular /([aeiou])s([A-Z]|_|$)/, '\1\2'
+  inflect.singular /([rlnd])es([A-Z]|_|$)/, '\1\2'
+  inflect.singular /([aeiou])s([A-Z]|_)([a-z]+)([rlnd])es($)/, '\1\2\3\4\5'
+  inflect.singular /([rlnd])es([A-Z]|_)([a-z]+)([aeiou])s($)/, '\1\2\3\4\5'
+  
+  inflect.irregular 'admin', 'admins'
+
+  # Para evitar errores de pluralización utilizados por formtastic al utilizar i18n
+  # (https://github.com/justinfrench/formtastic/blob/master/lib/formtastic.rb#L1850)
+  inflect.irregular 'label', 'labels'
+  inflect.irregular 'title', 'titles'
+  inflect.irregular 'hint', 'hints'
+  inflect.irregular 'action', 'actions'
+end
+#
 # These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
+# ActiveSupport::Inflector.inflections do |inflect|
 #   inflect.acronym 'RESTful'
 # end
